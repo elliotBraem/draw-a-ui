5c89edb1ebf7609a12f8f64982eceb7fc4c10388
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-11-14 16:30:27 -0500

    feat: working version with widget and near-bos-webcomponent

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 'use client'
-import Lockup from './lockup.svg'
 
-import dynamic from 'next/dynamic'
+import { useBreakpoint } from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
-import React, { useEffect, useState } from 'react'
+import dynamic from 'next/dynamic'
 import { PreviewShapeUtil } from './PreviewShape/PreviewShape'
-import { ExportButton } from './components/ExportButton'
-import { useBreakpoint, useDialogs, useEditor } from '@tldraw/tldraw'
 import { APIKeyInput } from './components/APIKeyInput'
+import { ExportButton } from './components/ExportButton'
 
 const Tldraw = dynamic(async () => (await import('@tldraw/tldraw')).Tldraw, {
 	ssr: false,
@@ -33,7 +31,7 @@ export default function Home() {
 	)
 }
 
-function LockupLink() {
+function LockupLink() { // whoa what is this
 	const breakpoint = useBreakpoint()
 	return (
 		<a
